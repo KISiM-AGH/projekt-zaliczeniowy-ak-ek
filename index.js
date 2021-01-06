3bf0aad3053881268f0da0b3a4c00fb037b8e83e
perkele4040 <emil_kobylecki@onet.eu>
2021-01-06 16:50:17 +0100

    przygotowanie do bazy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 9000;
+const port = process.env.PORT || 9000;
 const env = process.env.NODE_ENV || 'development';
 const app = express();
 app.use(express.json());
