cb8f8ed794de81ba2156d0e081dc632b7fd6a5d9
agusko <agnieszka7867@wp.pl>
2021-01-07 22:51:49 +0100

    added sudoku folder

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const api = require('./src/api')
-
+const api = require('./src/api');
 const port = process.env.PORT || 9000;
 const env = process.env.NODE_ENV || 'development';
 const app = express();
 app.use(express.json());
 app.use('/api', api)
-
-
-
 
 app.get('/', (req, res) =>{
     res.send({msg: 'Witam serdecznie ze szkieletu generatora sudoku REST, wprowadzam delikatną zmianę'})
@@ -27,6 +23,11 @@ app.get('/User', (req, res) => {
     res.send(dbUSER);
 })
 */
+app.get('/sudoku', (req, res) => {
+    celu();
+    console.log(sudoku.generate("easy"));
+    res.send(sudoku.generate("easy").toString());
+})
 app.listen(port, '127.0.0.1',() =>{
     console.log(`Server listening on http://127.0.0.1:${port} in ${env} mode`);
 } );
